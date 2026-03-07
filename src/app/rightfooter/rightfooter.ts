1e28971ca6e0c07b9a341530877ec4e52902f6a4
Khrystyna Pyniaha <pynyaga.kristina@gmail.com>
2026-03-07 11:17:42 +0200

    @if and @for for righfooter.html and leftfooter.html

--- src/app/rightfooter/rightfooter.ts
+++ src/app/rightfooter/rightfooter.ts
@@ -6,4 +6,29 @@ import { Component } from '@angular/core';
   templateUrl: './rightfooter.html',
   styleUrl: './rightfooter.scss',
 })
-export class Rightfooter {}
+export class Rightfooter {
+
+  showReferences: boolean = true;
+  showInterests: boolean = true;
+
+  references = [
+    {
+      name: "Glenin M. Gregory",
+      position: "Director Marix Media Ltd.",
+      phone: "+555 987 093"
+    },
+    {
+      name: "Jennifer S. Gavin",
+      position: "Director Marix Media Ltd.",
+      phone: "+555 123 454"
+    }
+];
+
+  interests = [
+  { name: "Travel", icon: "fas fa-plane" },
+  { name: "Music", icon: "fas fa-music" },
+  { name: "Writing", icon: "fas fa-pen-nib" },
+  { name: "Chess", icon: "fas fa-chess" }
+];
+
+}
